a13a395d30a1245b873af20fa70fa6c3bc52ad40
HE Junyang <nathanhejunyang@gmail.com>
2017-08-24 13:43:20 +0200

    Added rpi temp & humi data recorder

---
+++ TempHumiChart_RpiRecorder/ex.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#source $HOME/.bash_profile
+python /path/to/TempHumiChart.py 22 15 | tee -a /path/to/data.txt /path/to/dataDashBoard.txt
+
+dt=$(awk -F';' 'END { print $1 }' /home/pi/Adafruit_Python_DHT/data/dataDashBoard.txt);
+t=$(awk -F';' 'END { print $2 }' /home/pi/Adafruit_Python_DHT/data/dataDashBoard.txt);
+h=$(awk -F';' 'END { print $3 }' /home/pi/Adafruit_Python_DHT/data/dataDashBoard.txt);
+
+url="https://catprogrammer.com/DashBoard/uploadData.php?dt=";
+url=${url}${dt};
+url=${url}"&t=";
+url=${url}${t};
+url=${url}"&h=";
+url=${url}${h};
+
+url=$(echo $url | sed -e "s/ /+/");
+
+curl $url;
